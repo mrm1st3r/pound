05a36999a8fed442da12619d7254478ffc4b8af1
Reinhardt, Martin <contact@martinreinhardt-online.de>
2018-02-08 10:23:20 +0100

    feat(testing): Switch to Chrome Headless

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,37 +1,61 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+const path = require('path');
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['PhantomJS'],
-    singleRun: false,
-    phantomjsLauncher: {
-      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
-      exitOnResourceError: true
-    }
-  });
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular/cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-remap-istanbul'),
+            require('@angular/cli/plugins/karma'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-junit-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular/cli/plugins/karma')
+        ],
+        client: {
+            clearContext: false
+        },
+        files: [
+            { pattern: './src/test/frontend/setup.ts', watched: false }
+        ],
+        preprocessors: {
+            './src/test/frontend/setup.ts': ['@angular/cli']
+        },
+        coverageReporter: {
+            type: 'in-memory'
+        },
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly', 'text-summary'],
+            fixWebpackSourcePaths: true,
+            dir: path.join(__dirname, 'target/ccoverage'),
+        },
+        junitReporter: {
+            title: 'NexTrust-WebApp',
+            outputDir: path.join(__dirname, 'target/surefire-reports')
+        },
+        angularCli: {
+            environment: 'dev'
+        },
+        customLaunchers: {
+            ChromeHeadless: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    '--headless',
+                    '--disable-gpu',
+                    '--remote-debugging-port=9222',
+                ]
+            }
+        },
+        browserNoActivityTimeout: 120000,
+        reporters: config.angularCli && config.angularCli.codeCoverage ? ['progress', 'junit', 'coverage-istanbul'] : ['progress', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+    });
 };
